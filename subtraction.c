8544439ed56dc8a56dff9efd97e313dd5c05df8c
Samiksha Ande <samiksha@thoughtworks.com>
2016-07-31 14:52:10 +0530

    Added subtraction for two numbers

---
+++ subtraction.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+ 
+int main()
+{
+   int first, second;
+first = 10;
+second = 5;
+ 
+   subtract = first - second;
+ 
+   printf("Difference = %d\n",subtract);
+ 
+   return 0;
+}
